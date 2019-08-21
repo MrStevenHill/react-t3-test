71c87eaa523769e4023752a0db811cea05e4b254
Steven Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-08-21 14:40:52 +0100

    Added simple test examples

---
+++ src/Simple.test.js
@@ -0,0 +1,34 @@
+function sum(a, b) {
+  return a + b;
+}
+
+// Equality
+test('sum of 1 & 2 equals 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+// Inequality
+test('sum of 1 & 2 does not equal 4', () => {
+  expect(sum(1, 2)).not.toBe(4);
+});
+
+// Regular Expressions
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+
+// Arrays
+const shoppingList = [
+  'diapers',
+  'kleenex',
+  'trash bags',
+  'paper towels',
+  'beer',
+];
+
+test('the shopping list has beer on it', () => {
+  expect(shoppingList).toContain('beer');
+});
+
+// See Jest docs for more
+// https://jestjs.io/docs/en/using-matchers
