bb904a68716563839440b10dd87ba60497239672
Steven Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-08-27 09:57:33 +0100

    Change name to be more meaningful

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const container = document.createElement('div');
+  ReactDOM.render(<App />, container);
+  ReactDOM.unmountComponentAtNode(container);
 });
